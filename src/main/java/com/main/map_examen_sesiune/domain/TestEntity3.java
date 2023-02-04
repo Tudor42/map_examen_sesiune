4fb5c8084e2a9d2070711ae495841aa7757448b4
Tudor42 <psucas.tudor42@gmail.com>
2023-02-04 20:58:58 +0200

    TestEntity3 modif

--- src/main/java/com/main/map_examen_sesiune/domain/TestEntity3.java
+++ src/main/java/com/main/map_examen_sesiune/domain/TestEntity3.java
@@ -1,7 +1,6 @@
 package com.main.map_examen_sesiune.domain;
 
 import com.main.map_examen_sesiune.ORM.annotations.columntype.ForeignKey;
-
 public class TestEntity3 extends TestBaseClass{
     @ForeignKey(entity = TestEntity2.class)
     int fkTestEntity1;
