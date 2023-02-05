dfe4f2a0e68b925ff5515c577d93900d53b38d06
Tudor42 <psucas.tudor42@gmail.com>
2023-02-05 02:56:08 +0200

    getEntitiesWithProps script creation bug solved

--- src/main/java/com/main/map_examen_sesiune/ORM/sql/GetEntityScript.java
+++ src/main/java/com/main/map_examen_sesiune/ORM/sql/GetEntityScript.java
@@ -1,18 +1,19 @@
 package com.main.map_examen_sesiune.ORM.sql;
 
 import com.main.map_examen_sesiune.ORM.classparser.FieldsParser;
+import com.main.map_examen_sesiune.ORM.exceptions.OrmException;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GetEntityScript {
-    public static HashMap<Integer, Object> getEntityScript(Object o) throws IllegalAccessException {
+    public static HashMap<Integer, Object> getEntityScript(Object o) throws IllegalAccessException, OrmException {
         // Get entity by primary key
         return getEntitiesScript(o, FieldsParser.getPkFields(o.getClass()));
     }
     public static HashMap<Integer, Object> getEntitiesScript(Object props, ArrayList<Field> fields)
-            throws IllegalAccessException {
+            throws IllegalAccessException, OrmException {
         // Get entities with props
         HashMap<Integer, Object> result = new HashMap<>();
         int nextPlaceHolderIndex = 1;
@@ -20,10 +21,10 @@ public class GetEntityScript {
         script.append(props.getClass().getSimpleName().toLowerCase()).append(" WHERE ");
         for(Field f: fields){
             script.append(f.getName().toLowerCase()).append("=? AND ");
-            f.setAccessible(true);
-            result.put(nextPlaceHolderIndex, f.get(props));
+        }
+        for(Object o: FieldsParser.getAllFieldsValues(props, fields)){
+            result.put(nextPlaceHolderIndex, o);
             nextPlaceHolderIndex += 1;
-            f.setAccessible(false);
         }
         script.setLength(script.length()-4);
         result.put(0, script.toString());
